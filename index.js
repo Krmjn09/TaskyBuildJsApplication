76f2d8c723811f6493ea6eecac6db001cfb0fc7b
Krmjn09 <kmahajan1_be22@thapar.edu>
2024-01-01 18:40:53 +0530

    changes committed

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log("connected");
+// var state = {
+    // tasklist : [
+    //     {
+    //         imageUrl: "",
+    //         taskTitle: "",
+    //         taskType: "",
+    //         taskDescription: "",
+    //     },
+    //      {
+    //         imageUrl: "",
+    //         taskTitle: "",
+    //         taskType: "",
+    //         taskDescription: "",
+    //     },
+    //      {
+    //         imageUrl: "",
+    //         taskTitle: "",
+    //         taskType: "",
+    //         taskDescription: "",
+    //     },
+    //      {
+    //         imageUrl: "",
+    //         taskTitle: "",
+    //         taskType: "",
+    //         taskDescription: "",
+    //     },
+    // ]
+// }
+const state = {
+    taskList: [],
+};
+
+// DOM operations
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
+
+
+// console.log(taskContents);
+// console.log(taskModal);
+
+
+//Template for cards on screen
+const htmlTaskContent = ({ id, title, Description, type, url}) => `
+<div class='col-md-6 col-lg-4 mt-3' id=${id}>
+<div class='card '>
+</div>
+</div>
+`;
